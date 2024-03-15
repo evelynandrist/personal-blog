9ef4d79c1f0d49d4e23de850d18817bf8038491a
Kevin Bam <kevin1bam@web.de>
2024-03-15 12:41:17 +0100

    initialize repo from template

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "example-node-js-flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in rec {
+        flakedPkgs = pkgs;
+
+        # enables use of `nix shell`
+        devShell = pkgs.mkShell {
+          # add things you want in your shell here
+          buildInputs = with pkgs; [
+            nodejs
+	    yarn
+          ];
+          shellHook = "yarn; zsh; exit";
+        };
+      }
+    );
+}
